5560c115f6d2500a4bbdd3bf3b7223dcba2c52e1
carmart14 <139304795+carmart14@users.noreply.github.com>
2025-11-29 15:33:05 -0500

    adding more visual analysis

--- FinalProject/data/netflix_analysis.R
+++ FinalProject/data/netflix_analysis.R
@@ -7,6 +7,7 @@
 
 library(tidyverse)
 library(ggplot2)
+library(dplyr)
 
 
 data_user <- read.csv(file = "/Users/carmart/Downloads/DS611/FinalProject/Netflix_UserInfo/netflix_users.csv")
@@ -33,7 +34,7 @@ content_count <- data_movsho |>
 
 ggplot(content_count, aes(x = Country, y = count, fill = Type)) +
   geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = c("Movie" = "green", "TV Show" = "blue")) +
+  scale_fill_manual(values = c("Movie" = "red", "TV Show" = "blue")) +
   labs(title = "Count of Movies and TV Shows by Country",
        x = "Country",
        y = "Count",
@@ -42,19 +43,72 @@ ggplot(content_count, aes(x = Country, y = count, fill = Type)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
-ggplot(content_sumary, aes(x = Country, y = count, fill = Genre)) +
+ggplot(content_sumary, aes(x = Type, y = count, fill = Type)) +
   geom_col(position = "dodge") +
-  facet_wrap(~ Type) +
+  facet_grid(Country ~ Genre) +
   labs(
-    title = "Most Common Genres by Country and Type",
-    x = "Genre",
+    title = "Genre Popularity by Country (Movies vs TV Shows)",
+    x = "Type",
     y = "Count"
   ) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    strip.text = element_text(size = 8)
+  ) +
+  guides(fill = "none")
+
+# same as above but simpler so put into props not raw values
+
+genre_props <- content_sumary %>%
+  group_by(Country, Type) %>%
+  mutate(prop = count / sum(count)) %>%
+  ungroup()
+
+ggplot(genre_props, aes(x = Genre, y = prop, fill = Type)) +
+  geom_col(position = "dodge",) +
+  facet_wrap(~ Country, scales = "free_x") +
+  labs(title = "Genre Preference Proportions by Country",
+       y = "Proportion")
+
+# prefernce scoring 
+
+pref_score <- content_sumary |> 
+  pivot_wider(names_from = Type, values_from = count, values_fill = 0) |> 
+  mutate(
+    preference = (Movie - `TV Show`) / (Movie + `TV Show`) # keep the backticks to account for the space char
+  )
+
+ggplot(pref_score, aes(x = Country, y = Genre, fill = preference)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red") +
+  labs(title = "Movie vs TV Genre Preference Score")
 
 
-top_genre_per_country <- content_sumary |> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+top_genre_per_cCountrytop_genre_per_country <- content_sumary |> 
   group_by(Country, Type) |> 
   slice_max(count, n = 2)
 
